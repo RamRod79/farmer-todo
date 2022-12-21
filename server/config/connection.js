769389cab828a2b2dc38ca648a7837a6ad61613b
Armando.Rod <armando.rod79@gmail.com>
2022-12-20 20:06:06 -0800

    update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
 
-// MONGODB_URI = 'mongodb+srv://Ramrod:MerryChristmas@farmertodo.hncaqbo.mongodb.net/?retryWrites=true&w=majority'
+// const MONGOD_URI = MONGODB_URI = 'mongodb+srv://Ramrod:MerryChristmas@cluster0.xactu5z.mongodb.net/?retryWrites=true&w=majority'
+
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/project3', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
+mongoose.connection.on('connected', () => {
+	console.log('MDB is connected')
+})
 
 module.exports = mongoose.connection;
